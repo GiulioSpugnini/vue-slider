7408c7b023494614ce57fabd30ddb7955f40b5f0
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-27 17:25:46 +0100

    fixed functions

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ const app = new Vue({
         images: ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg', 'images/image4.jpg'],
     },
     methods: {
-        nextImg(images) {
-            this.currentIndex === images.length - 1 ? this.currentIndex === 0 : this.currentIndex++;
+        nextImg() {
+            this.currentIndex === this.images.length - 1 ? this.currentIndex === 0 : this.currentIndex++;
         },
-        prevImg(images) {
-            this.currentIndex === 0 ? this.currentIndex === images.length - 1 : this.currentIndex--;
+        prevImg() {
+            this.currentIndex === 0 ? this.currentIndex === this.images.length - 1 : this.currentIndex--;
         },
-        setImg() {
+        setImg(index) {
             this.currentIndex = index;
         },
         isActive(index) {
