54a317fd7bb960ab6e27e7f0bd5de81421eec6b2
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-27 16:27:01 +0100

    binded the images

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-console.log('JS OK');
+console.log('Vue OK', Vue);
 Vue.config.devtools = true;
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        currentIndex: 0,
+        images: ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg', 'images/image4.jpg'],
+    },
+    methods: {
+
+    },
+});
